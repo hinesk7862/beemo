71a49bad168589a87809c09bc3a07459247b645b
hinesk7862 <kevin.hines7862@gmail.com>
2024-11-11 18:21:37 -0500

    Update hdmi_test.py

--- hdmi_test.py
+++ hdmi_test.py
@@ -7,12 +7,12 @@ os.system ("TERM=linux setterm -foreground black -clear all >/dev/tty0")
 # this is the frambuffer for analog video output - note that this is a 16 bit RGB
 # other setups will likely have a different format and dimensions which you can check with
 # fbset -fb /dev/fb0 
-buf = np.memmap('/dev/fb0', dtype='uint16',mode='w+', shape=(576,720))
+buf = np.memmap('/dev/fb0', dtype='uint16',mode='w+', shape=(1200,720))
 
 print(buf)
 # fill with white
 
-b = np.full((576, 720), 0x8f51)
+b = np.full((1200, 720), 0x8f51)
 
 while True:
     buf[:] = b
