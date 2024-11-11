781ebc1320cb73658fe703904bc8d472eac5e432
hinesk7862 <kevin.hines7862@gmail.com>
2024-11-11 18:30:02 -0500

    Update hdmi_test.py

--- hdmi_test.py
+++ hdmi_test.py
@@ -5,7 +5,7 @@ import os
 os.system ("TERM=linux setterm -foreground black -clear all >/dev/tty0")
 
 screen_width = 2160
-screen_height = 1440
+screen_height = 1080
 
 # this is the frambuffer for analog video output - note that this is a 16 bit RGB
 # other setups will likely have a different format and dimensions which you can check with
