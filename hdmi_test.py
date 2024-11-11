1d654686df7283d09a42fa650fcfb6292d5013d2
hinesk7862 <kevin.hines7862@gmail.com>
2024-11-11 18:29:42 -0500

    Update hdmi_test.py

--- hdmi_test.py
+++ hdmi_test.py
@@ -4,8 +4,8 @@ import os
 # this turns off the cursor blink:
 os.system ("TERM=linux setterm -foreground black -clear all >/dev/tty0")
 
-screen_width = 100
-screen_height = 800
+screen_width = 2160
+screen_height = 1440
 
 # this is the frambuffer for analog video output - note that this is a 16 bit RGB
 # other setups will likely have a different format and dimensions which you can check with
@@ -19,8 +19,4 @@ b = np.full((screen_height,screen_width), 0xaef5)
 
 while True:
     buf[:] = b
-    buf[200:100] = 0x31c6
-    buf[201:100] = 0x31c6
-    buf[201:101] = 0x31c6
-    buf[200:101] = 0x31c6
 
