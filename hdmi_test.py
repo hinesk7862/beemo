163c6c55574c6bbc6ccb18356d6086a35f2987d4
hinesk7862 <kevin.hines7862@gmail.com>
2024-11-11 18:15:46 -0500

    Update hdmi_test.py

--- hdmi_test.py
+++ hdmi_test.py
@@ -12,6 +12,6 @@ buf = np.memmap('/dev/fb0', dtype='uint16',mode='w+', shape=(576,720))
 print(buf)
 # fill with white
 
-for x in buf:
-    for y in x:
-        y = 0xbf17
+for xPos,x in enumerate(buf):
+    for yPos, y in enumerate(x):
+        buf[x,y] = 0xbf17
